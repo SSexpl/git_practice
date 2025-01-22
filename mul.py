616dd0ec5bee8c322e22558ef0548e12e1ef86d0
SSexpl <87913198+SSexpl@users.noreply.github.com>
2025-01-23 00:24:40 +0530

    addedmul

---
+++ mul.py
@@ -0,0 +1,14 @@
+# Program to add two numbers
+
+# Input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+result = num1  * num2
+
+# Display the result
+print(f"The mul of {num1} and {num2} is {result}")
+
+
+#updated mul file 
